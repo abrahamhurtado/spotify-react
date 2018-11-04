b5fd93d8864a96cda9e345255b95e85bf675ea4e
Abraham Hurtado <arhc.hc@gmail.com>
2018-11-04 14:39:00 -0700

    Refactoring TopMusic rendering

--- src/components/TopMusic.js
+++ src/components/TopMusic.js
@@ -1,8 +1,10 @@
 import React from "react";
 import bg from "../media/music2.jpg";
+import ChoiceCard from "./Elements/ChoiceCard";
+import ChoiceTitle from "./Elements/ChoiceTitle";
 import HeroBackground from "./Elements/HeroBackground";
-import HeroBox from "./Elements/HeroBox";
 import { Title } from "./Elements/HeroTitles";
+import List from "./Elements/List";
 
 const TopMusic = props => (
   <HeroBackground
@@ -16,9 +18,32 @@ const TopMusic = props => (
       opacity: 0.5
     }}
   >
-    <HeroBox>
-      <Title>Elige lo que quieres conocer</Title>
-    </HeroBox>
+    <Title>Elige lo que quieres conocer</Title>
+    <List horizontal>
+      <ChoiceCard
+        gradientStyles={{
+          gradientFallback: "#667db6",
+          gradientFallbackWebkit:
+            "-webkit-linear-gradient(to left, #667db6, #0082c8, #0082c8, #667db6)",
+          gradient:
+            "linear-gradient(to left, #667db6, #0082c8, #0082c8, #667db6)",
+          opacity: 0.5
+        }}
+      >
+        <ChoiceTitle>Mis artistas más escuchados</ChoiceTitle>
+      </ChoiceCard>
+      <ChoiceCard
+        gradientStyles={{
+          gradientFallback: "#ff4b1f",
+          gradientFallbackWebkit:
+            "-webkit-linear-gradient(to bottom, #ff4b1f, #1fddff)",
+          gradient: "linear-gradient(to bottom, #ff4b1f, #1fddff)",
+          opacity: 0.5
+        }}
+      >
+        <ChoiceTitle>Mis canciones más escuchadas</ChoiceTitle>
+      </ChoiceCard>
+    </List>
   </HeroBackground>
 );
 
