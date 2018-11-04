05c6155724b09fc9a7dcc1a48a84b60621de8759
Abraham Hurtado <arhc.hc@gmail.com>
2018-11-04 13:39:25 -0700

    Implements awful hack to redirect from Authorized to TopMusic

--- src/components/Authorized.js
+++ src/components/Authorized.js
@@ -1,5 +1,8 @@
 import { Redirect } from "@reach/router";
 import React, { Component } from "react";
+import HeroBackground from "./Elements/HeroBackground";
+import HeroBox from "./Elements/HeroBox";
+import { Title } from "./Elements/HeroTitles";
 
 export default class Authorized extends Component {
   componentDidMount() {
@@ -18,11 +21,25 @@ export default class Authorized extends Component {
       localStorage.setItem("access_token", access_token);
       localStorage.setItem("expires_in", expires_in);
       localStorage.setItem("issued_at", Date.now());
+
+      setTimeout(() => {
+        this.props.navigate("/top");
+      }, 0);
     }
   }
   render() {
     if (this.props.location.hash) {
-      return <h2>Spotify ha dado su veredicto</h2>;
+      return (
+        <HeroBackground
+          gradientStyles={{
+            gradientFallback: "e53935"
+          }}
+        >
+          <HeroBox>
+            <Title>Spotify nos ha dado acceso</Title>
+          </HeroBox>
+        </HeroBackground>
+      );
     } else {
       return <Redirect to="/" noThrow />;
     }
