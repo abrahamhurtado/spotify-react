8cd7ad64ce66be5c66769e7355dde7c89b2e2f02
Abraham Hurtado <arhc.hc@gmail.com>
2018-11-04 01:13:49 -0700

    Adds requestSpotifyPermissions method on SpotifyButton component

--- src/components/SpotifyButton.js
+++ src/components/SpotifyButton.js
@@ -10,9 +10,28 @@ import {
 import Button from "./Elements/Button";
 
 export default class SpotifyButton extends Component {
+  requestSpotifyPermissions = e => {
+    e.preventDefault();
+    const requestParameters = {
+      client_id: "d49c7151491642d796b06c0bc16b2bf2",
+      response_type: "token",
+      redirect_uri: `${window.location.href}authorized`,
+      state: 1,
+      scope: "user-top-read"
+    };
+
+    const qs = Object.keys(requestParameters)
+      .map(key => `${key}=${requestParameters[key]}`)
+      .reduce((x, y) => `${x}&${y}`);
+
+    window.location = `https://accounts.spotify.com/authorize?${qs}`;
+  };
   render() {
     return (
-      <Button className={cx(spotifyButton, boxShadow, basicTransition)}>
+      <Button
+        className={cx(spotifyButton, boxShadow, basicTransition)}
+        onClick={this.requestSpotifyPermissions}
+      >
         <Spotify className={spotifyLogo} /> Entra con Spotify
       </Button>
     );
