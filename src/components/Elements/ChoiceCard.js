fc9fab87e516a2784d65d7222ba6c163ceed08c9
Abraham Hurtado <arhc.hc@gmail.com>
2018-11-04 14:33:21 -0700

    Adds ChoiceCard and ChoiceTitle components

---
+++ src/components/Elements/ChoiceCard.js
@@ -0,0 +1,50 @@
+import styled from "react-emotion";
+import { basicTransition, boxShadow } from "../../utils/classes";
+
+const ChoiceCard = styled("li")`
+  ${boxShadow};
+  ${basicTransition};
+  cursor: pointer;
+  list-style: none;
+  height: 200px;
+  width: 35%;
+  display: flex;
+  align-items: flex-end;
+  justify-content: flex-start;
+  text-align: left;
+  border-radius: 4px;
+  padding: 0.75rem 1rem;
+  position: relative;
+  & > * {
+    position: relative;
+  }
+  &::before {
+    content: " ";
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: 100%;
+    height: 100%;
+    opacity: ${props =>
+      props.gradientStyles && props.gradientStyles.opacity
+        ? props.gradientStyles.opacity
+        : "1"};
+    background: ${props =>
+      props.gradientStyles && props.gradientStyles.gradientFallback
+        ? props.gradientStyles.gradientFallback
+        : "transparent"};
+    background: ${props =>
+      props.gradientStyles && props.gradientStyles.gradientFallbackWebkit
+        ? props.gradientStyles.gradientFallbackWebkit
+        : "transparent"};
+    background: ${props =>
+      props.gradientStyles && props.gradientStyles.gradient
+        ? props.gradientStyles.gradient
+        : "transparent"};
+  }
+`;
+
+export default ChoiceCard;
---
+++ src/components/Elements/ChoiceTitle.js
@@ -0,0 +1,10 @@
+import styled from "react-emotion";
+
+const ChoiceTitle = styled("h3")`
+  color: #fff;
+  font-size: 1.25rem;
+  text-shadow: 0 2px 4px rgba(0, 0, 0, 0.25);
+  margin: 0;
+`;
+
+export default ChoiceTitle;
