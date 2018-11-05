3f1917dea92806356717a210e12b185a229ae4bf
Abraham Hurtado <arhc.hc@gmail.com>
2018-11-04 17:01:53 -0700

    App's component router embedds TopMusic router

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
       <Router>
         <Hero path="/" />
         <Authorized path="/authorized" />
-        <TopMusic path="/top" />
+        <TopMusic path="/top/*" />
       </Router>
     );
   }
