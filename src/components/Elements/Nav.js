755dfc8877efc42669b62564207e97f0d90beeb1
Abraham Hurtado <arhc.hc@gmail.com>
2018-11-04 17:00:28 -0700

    Adds Nav component

---
+++ src/components/Elements/Nav.js
@@ -0,0 +1,34 @@
+import styled from "react-emotion";
+
+const Nav = styled("nav")`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  background: #333;
+  h1 {
+    font-size: 1rem;
+    font-weight: normal;
+    margin: 0 !important;
+    flex: 1 0 auto;
+    padding: 0 1rem;
+  }
+  ul {
+    a {
+      width: 50%;
+      margin: 0 !important;
+      li {
+        height: auto;
+        max-width: 100%;
+        border-radius: 0px;
+        justify-content: center;
+        text-align: center;
+        h3 {
+          font-weight: 400;
+          font-size: 1rem;
+        }
+      }
+    }
+  }
+`;
+
+export default Nav;
